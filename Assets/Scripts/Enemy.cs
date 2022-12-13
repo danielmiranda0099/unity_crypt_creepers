2afe6a7fe38c661f0147f694b9f6975953db73a8
Daniel Miranda <danielmesa877@gmail.com>
2022-12-13 08:13:12 -0500

    [ADD] comportamiento basico de enemigo / busqueda del jugador

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] int health = 1;
+    [SerializeField] int speed = 1;
+
+    Vector2 direction;
+
+    Transform player;
+
     void Start()
     {
-        
+        player = FindObjectOfType<Player>().transform; //Busqueda dinamica (los enemigos se ban a crear 
+                                                    //dinamicamente en tiempo de ejecucuion por eso buiscan al player de este modo)
     }
 
     
     void Update()
     {
-        
+        direction = player.position - transform.position;//Obtener vector de direccion del jugador
+        transform.position += (Vector3)direction * Time.deltaTime * speed;
     }
 
-    //PUBLIC para acceder a este metodo
     public void TakeDamage() {
         health--;
     }
